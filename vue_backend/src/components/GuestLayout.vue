e00c32476032d09ea1eeabb19353ce8040b0bfae
NAHDI51 <hossainnahdi8051@gmail.com>
2023-11-23 16:52:22 -0500

    Added a template child for login pages

---
+++ vue_backend/src/components/GuestLayout.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+      <div class="sm:mx-auto sm:w-full sm:max-w-sm">  
+        <div>
+          <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-white-900"> <!-- Corrected class name -->
+            {{ title }}
+          </h2>
+        </div>
+        <slot></slot>
+      </div>
+    </div>
+  </template>
+  
+  <script setup>
+  const { title } = defineProps({
+    title: String
+  })
+  </script>
+  
+  <style scoped>
+  </style>
+  
