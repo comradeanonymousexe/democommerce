562bd3f556f040ad163e36a926d78aeb1dd505ba
NAHDI51 <hossainnahdi8051@gmail.com>
2023-11-23 16:51:58 -0500

    Added  template to be used, added <router-view> to ResetPassword

--- vue_backend/src/views/Login.vue
+++ vue_backend/src/views/Login.vue
@@ -1,17 +1,3 @@
-<!--
-  This example requires some changes to your config:
-  
-  ```
-  // tailwind.config.js
-  module.exports = {
-    // ...
-    plugins: [
-      // ...
-      require('@tailwindcss/forms'),
-    ],
-  }
-  ```
--->
 <template>
     <!--
       This example requires updating your template:
@@ -21,11 +7,7 @@
       <body class="h-full">
       ```
     -->
-    <div class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-      <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-        
-        <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-white-900">Sign in to your account</h2>
-      </div>
+    <GuestLayout title="Sign in to your account">
   
       <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
         <form class="space-y-6" action="#" method="POST">
@@ -40,7 +22,9 @@
             <div class="flex items-center justify-between">
               <label for="password" class="block text-sm font-medium leading-6 text-white-900">Password</label>
               <div class="text-sm">
-                <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</a>
+                <router-link :to="{name: 'requestPassword'}" class="font-semibold text-indigo-600 hover:text-indigo-500">
+                  Forgot password?
+                </router-link>
               </div>
             </div>
             <div class="mt-2">
@@ -59,9 +43,14 @@
           <a href="#" class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Start a 14 day free trial</a>
         </p>
       </div>
-    </div>
+    </GuestLayout>
   </template>
   
+<script setup>
+// IMports guest layout
+import GuestLayout from "../components/GuestLayout.vue";
+
+</script>
 
 <style scoped>
 
