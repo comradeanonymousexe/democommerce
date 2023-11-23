a3b402e85ceaf144ab6331b7dd1ff5497165aab4
NAHDI51 <hossainnahdi8051@gmail.com>
2023-11-23 16:49:56 -0500

    Added RequestPassword and ResetPassword routes

--- vue_backend/src/router/index.js
+++ vue_backend/src/router/index.js
@@ -4,6 +4,9 @@ import {createRouter, createWebHistory} from "vue-router";
 import Dashboard from "../views/Dashboard.vue";
 import Login from "../views/Login.vue";
 import Registration from "../views/Registration.vue";
+import RequestPassword from "../views/RequestPassword.vue";
+import ResetPassword from "../views/ResetPassword.vue";
+// import GuestLayout from "../components/GuestLayout.vue";
 
 
 const routes = [
@@ -17,16 +20,32 @@ const routes = [
         name: 'login',
         component: Login
     },
+    
     {
         path: '/registration',
         name: 'registration',
-        component: Registration
+        component: Registration,
+        
     },
+    
     {
-        path: '/request_password',
-        name: 'request_password',
-        component: RequestPassword
+        path: '/request-password',
+        name: 'requestPassword',
+        component: RequestPassword,
+        meta: {
+          requiresGuest: true
+        }
+    },
+    
+    {
+        path: '/reset-password/:token',
+        name: 'resetPassword',
+        component: ResetPassword,
+        meta: {
+          requiresGuest: true
+        }
     }
+    
 ];
 
 const router = createRouter({
