c604f3451e59f014a54ac528af82dbfbb94acbe6
NAHDI51 <hossainnahdi8051@gmail.com>
2023-11-25 22:02:54 -0500

    commented out the store variable

--- vue_backend/src/store/index.js
+++ vue_backend/src/store/index.js
@@ -3,7 +3,8 @@ import {createStore} from "vuex";
 const store = createStore({
     state: {
       user: {
-        token: '1234'
+        // token: '1234'
+        //token: null
       }
     },
     getters: {},
